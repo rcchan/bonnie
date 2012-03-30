6cf4d9974bb88a55f90340d9c8ccd7a07744365e
Andre Quina <aquina@mitre.org>
2012-03-30 16:12:14 -0400

    fixed changes to factory girl

--- test/factories/factory.rb
+++ test/factories/factory.rb
@@ -1,25 +1,33 @@
 require 'factory_girl'
 
-# ==========
-# = USERS =
-# ==========
-FactoryGirl.define :user do |u| 
-  u.sequence(:email) { |n| "testuser#{n}@test.com"} 
-  u.password 'password' 
-  u.password_confirmation 'password'
-  u.first_name 'first'
-  u.last_name 'last'
-  u.sequence(:username) { |n| "testuser#{n}"}
-  u.admin false
-  u.approved true
-  u.agree_license true
+
+FactoryGirl.define do
+  
+  # ==========
+  # = USERS =
+  # ==========
+  
+
+  factory :user do |u| 
+    u.sequence(:email) { |n| "testuser#{n}@test.com"} 
+    u.password 'password' 
+    u.password_confirmation 'password'
+    u.first_name 'first'
+    u.last_name 'last'
+    u.sequence(:username) { |n| "testuser#{n}"}
+    u.admin false
+    u.approved true
+    u.agree_license true
+  end
+
+  factory :admin, :parent => :user do |u|
+    u.admin true
+  end
+
+  factory :unapproved_user, :parent => :user do |u|
+    u.approved false
+  end
+  
 end
 
-FactoryGirl.define :admin, :parent => :user do |u|
-  u.admin true
-end
-
-FactoryGirl.define :unapproved_user, :parent => :user do |u|
-  u.approved false
-end
 
