269a0e236b082771272ef7747640199c4f1f448c
Andre Quina <aquina@mitre.org>
2012-04-20 08:46:36 -0400

    fixed unit tests

---
+++ test/unit/models/user_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  
+  setup do
+    dump_database
+    @user = FactoryGirl.create(:user)
+  end
+  
+  test "user should be found by username" do
+    found_user = User.by_username @user.username
+    assert_equal @user, found_user
+  end
+
+  test "user should be found by email" do
+    found_user = User.by_email @user.email
+    assert_equal @user, found_user
+  end
+  
+end
