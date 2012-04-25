5fd801e661e56c2344753326222af21edab3eed7
Andre Quina <aquina@mitre.org>
2012-04-25 17:36:56 -0400

    fixed loader

--- lib/measures/loader.rb
+++ lib/measures/loader.rb
@@ -11,7 +11,49 @@ module Measures
     end
     
     def load(measures)
+      measures = File.expand_path(File.join('.','test','fixtures','pophealth'))
+      measure_def = JSON.parse(File.open(File.join(measures,'0043.json')).read)
+
+      measure_js = File.open(File.expand_path(File.join('.','tmp','measures','NQF_0043.xml.js'))).read
       
+      map = "function() {
+        var patient = this;
+        var effective_date = <%= effective_date %>;
+
+        hqmfjs = {}
+        <%= init_js_frameworks %>
+        
+        var patient_api = new hQuery.Patient(patient);
+
+        // clear out logger
+        if (typeof Logger != 'undefined') Logger.logger = [];
+        // turn on logging if it is enabled
+        if (Logger.enabled) enableLogging();
+        
+        #{measure_js}
+        
+        var population = function() {
+          return hqmfjs.IPP(patient_api);
+        }
+        var denominator = function() {
+          return hqmfjs.DENOM(patient_api);
+        }
+        var numerator = function() {
+          return hqmfjs.NUMER(patient_api);
+        }
+        var exclusion = function() {
+          return false;
+        }
+        
+        if (Logger.enabled) enableMeasureLogging(hqmfjs);
+        
+        map(patient, population, denominator, numerator, exclusion);
+      };
+      "
+      
+      measure_def['map_fn'] = map
+      
+      bundle_def = JSON.parse(File.open(File.join(measures,'bundle.json')).read)
       measure_id = @db['measures'] << measure_def
       bundle_def['measures'] << measure_id
       bundle_id = @db['bundles'] << bundle_def
