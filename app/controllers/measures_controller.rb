a8466dbf021b1fa7a306ef2b335d24b622387399
squarism <milkfilk@gmail.com>
2012-05-04 13:54:10 -0400

    Really early debug console

--- app/controllers/measures_controller.rb
+++ app/controllers/measures_controller.rb
@@ -1,3 +1,13 @@
+class ErbContext < OpenStruct
+  def initialize(vars)
+    super(vars)
+  end
+  def get_binding
+    binding
+  end
+end
+
+
 class MeasuresController < ApplicationController
   
   layout :select_layout
@@ -111,9 +121,57 @@ class MeasuresController < ApplicationController
   
   def debug
     @measure = Measure.find(params[:id])
+    
+    # TODO: this is terrible, will refactor
+    # FileUtils.mkdir_p File.join(".","tmp",'measures')
+    hqmf_path = File.expand_path(File.join('.','test','fixtures','measure-defs',@measure.measure_id,"#{@measure.measure_id}.xml"))
+    codes_path = File.expand_path(File.join('.','test','fixtures','measure-defs',@measure.measure_id,"#{@measure.measure_id}.xls"))
+    filename = Pathname.new(hqmf_path).basename
+    
+    measure = Measures::Loader.load(hqmf_path, codes_path, nil, nil, false)
+    measure_js = Measures::Exporter.execution_logic(measure)
+
+    patient_file = File.expand_path('./test/fixtures/patients/francis_drake.json')
+    patient_json = File.read(patient_file)
+
+    @js = ""
+    library_functions = Measures::Exporter.library_functions
+    ['map_reduce_utils'].each do |function|
+      @js << "#{function}_js = function () { #{library_functions[function]} }\n"
+      @js << "#{function}_js();\n"
+    end
+    
+    @js << library_functions['hqmf_utils'] + "\n"
+
+    @js << "execute_measure = function(patient) {\n #{measure_js} \n}\n"
+    @js << "emitted = []; emit = function(id, value) { emitted.push(value); } \n"
+    @js << "ObjectId = function(id, value) { return 1; } \n"
+    
+    @js << "// #########################\n"
+    @js << "// ######### PATIENT #######\n"
+    @js << "// #########################\n\n"
+    
+    @js << "var patient = #{patient_json};\n"
+
+    # template_str = File.read(File.join('.','test','fixtures','html','test_measure.html.erb'))
+    # template = ERB.new(template_str, nil, '-', "_templ_html")
+    # params = {'measure_id' => filename}
+    # context = ErbContext.new(params)
+    # result = template.result(context.get_binding)
+    # 
+    # out_file = File.join(".","tmp",'measures',"#{filename}.html")
+    # File.open(out_file, 'w') do |f| 
+    #   f.write(result)
+    # end
+    # 
+    # puts "wrote test html to: #{out_file}"
+    
+    
     # render :json => @measure.to_json
-    puts "oh hai"
     render "measures/debug"
+    
+    
+    
   end
   
 end
